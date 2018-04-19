fc158137951f430c2931a2a576f3a071204e9728
samuelclerod <samuelclerod@gmail.com>
2018-04-18 21:32:50 -0300

    iniciada impl. da fila com ES6

--- js/queue.js
+++ js/queue.js
@@ -1,25 +1,19 @@
-function Queue() {
-    var items = [];
-    this.enqueue = function(element) {
-        items.push(element);
-    };
-    this.dequeue = function() {
-        return items.shift();
-    };
-    this.front = function() {
-        return items[0];
-    };
-    this.isEmpty = function() {
-        return items.length == 0;
+class Queue{
+    constructor(){
+        this.items = [];
     }
-    this.size = function() {
-        return items.length;
+    enqueue(element) {
     }
-    this.clear = function() {
-        items = []
+    dequeue(){
     }
-    this.print = function(separator) {
-        if (!separator) separator = '';
-        return items.join(separator);
+    front() {
+    }
+    isEmpty() {
+    }
+    size() {
+    }
+    clear() {
+    }
+    print(separator=" - ") {
     }
 }
