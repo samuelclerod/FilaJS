1362dc32925263a81d13977399006723373988d4
samuelclerod <samuelclerod@gmail.com>
2018-10-09 17:17:43 -0300

    Implementa Filas

--- js/dynamic_queue.js
+++ js/dynamic_queue.js
@@ -1,12 +1,47 @@
 class DynamicQueue{
     constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
-    enqueue(element) {}
-    dequeue(){}
-    front() {}
-    back(){}
-    isEmpty() {}
-    size() {}
-    clear() {}
+    enqueue(element) {
+        let node = new Node(element);
+        if(this.isEmpty()){
+            this.head=this.tail=node;
+        }else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
+    dequeue(){
+        if(this.isEmpty()) return null;
+        let dead = this.head;
+        if(this.length===1){
+            this.head=this.tail=null;
+        }else{
+            this.head = dead.next;
+            dead.next=null;
+        }
+        this.length--;
+        return dead.element;
+    }
+    front() {
+        if(this.isEmpty()) return -1;
+        return this.head.element;
+    }
+    back(){
+        if(this.isEmpty()) return -1;
+        return this.tail.element;
+    }
+    isEmpty() {
+        return this.tail==null;
+    }
+    size() {
+        return this.length;
+    }
+    clear() {
+        this.tail=this.head= null;
+    }
     print(separator=" - ") {}
 }
