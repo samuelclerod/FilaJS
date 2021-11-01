47eae92eed7a47136f3b5250a033899f49dbe58e
Samuel Rodrigues <samuelclerod@gmail.com>
2021-11-01 20:52:48 -0300

    implemente static queue

--- js/main.js
+++ js/main.js
@@ -1,40 +1,44 @@
 var queue = new StaticQueue();
-$().ready(function() {
-
-    $('#enqueue').on('click', enqueueElement);
-    $('#item').keydown(function(e) {
-        //verify if enter was pressed
-        if (e.which == 13) enqueueElement();
-    });
-    $('#dequeue').on('click', dequeueElement);
-    $('#clear').on('click', clearQueue);
+$().ready(function () {
+  $("#enqueue").on("click", enqueueElement);
+  $("#item").keydown(function (e) {
+    //verify if enter was pressed
+    if (e.which == 13) enqueueElement();
+  });
+  $("#dequeue").on("click", dequeueElement);
+  $("#clear").on("click", clearQueue);
 });
 
 function enqueueElement() {
-    var item = $('#item').val();
-    if (item) {
-        queue.enqueue(item);
-        $('#item').val('');
-        showQueue();
-    }
+  var item = $("#item").val();
+  if (item) {
+    queue.enqueue(item);
+    $("#item").val("");
+    showQueue();
+  }
 }
 
 function dequeueElement() {
-    if (queue.isEmpty() == false) {
-        alert('Foi removido o elemento ' + queue.dequeue());
-        showQueue();
-    } else {
-        alert('Fila Vazia');
-    }
+  if (queue.isEmpty() == false) {
+    alert("Foi removido o elemento " + queue.dequeue());
+    showQueue();
+  } else {
+    alert("Fila Vazia");
+  }
 }
 
 function clearQueue() {
-    queue.clear();
-    showQueue();
+  queue.clear();
+  showQueue();
 }
 
 function showQueue() {
-    $('#output').empty();
-    $('#output').append('<div class="ui label">' +
-        queue.print('</div><div class="ui label">') + '</div></div>');
+  $("#output").empty();
+  if (!queue.isEmpty()) {
+    $("#output").append(
+      '<div class="ui label">' +
+        queue.print('</div><div class="ui label">') +
+        "</div></div>"
+    );
+  }
 }
--- js/static_queue.js
+++ js/static_queue.js
@@ -1,20 +1,45 @@
-class StaticQueue{
-    constructor(){
+class StaticQueue {
+  constructor() {
+    this.data = [];
+  }
+  enqueue(element) {
+    if (this.isEmpty()) {
+      this.data.push(element);
+    } else {
+      this.data.push(element);
     }
-    enqueue(element) {
+  }
+
+  dequeue() {
+    if (this.isEmpty()) throw new Error("Fila Vazia!!");
+    return this.data.shift();
+  }
+
+  front() {
+    if (!this.isEmpty()) {
+      return this.data[0];
     }
-    dequeue(){
+  }
+
+  back() {
+    if (!this.isEmpty()) {
+      return this.data[this.data.length - 1];
     }
-    front() {
-    }
-    back(){
-    }
-    isEmpty() {
-    }
-    size() {
-    }
-    clear() {
-    }
-    print(separator=" - ") {
-    }
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  size() {
+    return this.data.length;
+  }
+
+  clear() {
+    this.data = [];
+  }
+
+  print(separator = " - ") {
+    return this.data.join(separator);
+  }
 }
